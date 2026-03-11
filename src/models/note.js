e9d37bb12555e3d08b7b37b48357dffdf39e804c
ivanna_shchokalo <ivanka.m78@gmail.com>
2026-03-11 12:30:22 +0100

    hw 3

--- src/models/note.js
+++ src/models/note.js
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import { TAGS } from '../constants/tags';
 
 const noteSchema = new Schema(
   {
@@ -16,18 +17,7 @@ const noteSchema = new Schema(
 
     tag: {
       type: String,
-      enum: [
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo',
-      ],
+      enum: TAGS,
       default: 'Todo',
     },
   },
@@ -35,5 +25,10 @@ const noteSchema = new Schema(
     timestamps: true,
   },
 );
+
+noteSchema.index({
+  title: 'text',
+  content: 'text',
+});
 
 export const Note = model('Note', noteSchema);
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -6,13 +6,20 @@ import {
   getNoteById,
   updateNote,
 } from '../controllers/notesController.js';
+import { celebrate } from 'celebrate';
+import {
+  createNoteSchema,
+  getAllNotesSchema,
+  noteIdSchema,
+  updateNoteSchema,
+} from '../validations/notesValidation.js';
 
 const router = Router();
 
-router.get('/notes', getAllNotes);
-router.post('/notes', createNote);
-router.get('/notes/:noteId', getNoteById);
-router.delete('/notes/:noteId', deleteNote);
-router.patch('/notes/:noteId', updateNote);
+router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
+router.post('/notes', celebrate(createNoteSchema), createNote);
+router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
+router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
+router.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote);
 
 export default router;
